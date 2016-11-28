5b94b77dce7cde30b944314409345c4701c64fbd
Taiki Ono <taiks.4559@gmail.com>
2016-11-28 13:45:58 +0900

    Create base error class

--- lib/pact_expectations.rb
+++ lib/pact_expectations.rb
@@ -3,6 +3,48 @@ require "pact/support"
 require "set"
 
 class PactExpectations
+  class Error < ::StandardError
+  end
+  class NotFound < Error
+  end
+
+  class DuplicatedKey < Error
+  end
+
+  class VerifyError < Error
+    def initialize(not_call_responses = [], not_call_reificated = [])
+      @not_call_responses = not_call_responses
+      @not_call_reificated = not_call_reificated
+    end
+
+    def message
+      message = String.new("\n")
+      unless @not_call_responses.empty?
+        message << create_not_call_message(
+          "Some expectations were defined but not used to construct the Contract",
+          @not_call_responses
+        )
+      end
+      unless @not_call_reificated.empty?
+        message << create_not_call_message(
+          "Some expectations were defined but not used to stub Remote Facade",
+          @not_call_reificated
+        )
+      end
+      message
+    end
+
+    private
+
+    def create_not_call_message(title, list)
+      message = String.new("#{title}:\n")
+      list.each do |key|
+        message << "- #{key}\n"
+      end
+      message << "\n"
+    end
+  end
+
   class << self
     def add_response_body_for(key, expectation = {})
       raise DuplicatedKey if expectations.include?(key)
@@ -49,40 +91,5 @@ class PactExpectations
     def reificated_call
       @reificated_call ||= Set.new
     end
-  end
-end
-class PactExpectations::NotFound < StandardError; end
-class PactExpectations::DuplicatedKey < StandardError; end
-class PactExpectations::VerifyError < StandardError
-  def initialize(not_call_responses = [], not_call_reificated = [])
-    @not_call_responses = not_call_responses
-    @not_call_reificated = not_call_reificated
-  end
-
-  def message
-    message = String.new("\n")
-    unless @not_call_responses.empty?
-      message << create_not_call_message(
-        "Some expectations were defined but not used to construct the Contract",
-        @not_call_responses
-      )
-    end
-    unless @not_call_reificated.empty?
-      message << create_not_call_message(
-        "Some expectations were defined but not used to stub Remote Facade",
-        @not_call_reificated
-      )
-    end
-    message
-  end
-
-  private
-
-  def create_not_call_message(title, list)
-    message = String.new("#{title}:\n")
-    list.each do |key|
-      message << "- #{key}\n"
-    end
-    message << "\n"
   end
 end
